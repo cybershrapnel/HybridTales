e1ffb81698d3491a98a0fc19e40efab15fdc4bf9
nanocheeze <cybershrapnel@nanocheeze.com>
2023-06-23 10:35:22 -0700

    Update main.py
    added stories methods

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ app.add_middleware(
 
 app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
+# Keep track of todo's and stories. Does not persist if Python session is restarted.
 _TODOS: Dict[str, List[str]] = {}
+_STORIES: Dict[str, Dict[str, str]] = {}
 
 class TodoItem(BaseModel):
     todo: str
@@ -32,12 +33,20 @@ class TodoItem(BaseModel):
 class TodoIndex(BaseModel):
     todo_idx: int
 
+class StoryItem(BaseModel):
+    title: str
+    content: str
+
 @app.post("/todos/{username}", status_code=201)
 async def add_todo(username: str, todo: TodoItem) -> Dict[str, str]:
     if username not in _TODOS:
         _TODOS[username] = []
     _TODOS[username].append(todo.todo)
     return {"status": 'OK'}
+
+@app.get("/stories", response_model=List[StoryItem])
+async def get_stories() -> List[Dict[str, str]]:
+    return list(_STORIES.values())
 
 @app.get("/todos/{username}", response_model=List[str])
 async def get_todos(username: str) -> List[str]:
@@ -50,6 +59,18 @@ async def delete_todo(username: str, todo_index: TodoIndex):
         todos.pop(todo_index.todo_idx)
     else:
         raise HTTPException(status_code=404, detail="Todo not found")
+
+@app.post("/stories", status_code=201)
+async def add_story(story: StoryItem) -> Dict[str, str]:
+    _STORIES[story.title] = {"title": story.title, "content": story.content}
+    return {"status": 'OK'}
+
+@app.get("/stories/{storyId}", response_model=StoryItem)
+async def get_story(storyId: str) -> Dict[str, str]:
+    story = _STORIES.get(storyId)
+    if story is None:
+        raise HTTPException(status_code=404, detail="Story not found")
+    return story
 
 @app.get("/logo.png")
 async def plugin_logo() -> FileResponse:
